4b702367825f7ebdecf9f360dbc13848f8f4c59f
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-01-08 17:55:55 +0900

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenLocal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+rootProject.name = "Smart"
+
+// include("Smart-API") // Seperate API. The API Will Be Seperated if the Smart-API is Complete.
+include("Smart-BukkitAPI") // an Official Name For Legacy API Updates.
+// include("Smart-PaperAPI") // In case PaperMC Split.
+include("Smart-Server") // The Server File. 
