75c56af859812e569376de4fbb1fd3b27c7fbb34
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-01-08 17:58:04 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
-rootProject.name = "Smart"
+rootProject.name = "ApproximasterSmartServer2004"
 
 // include("Smart-API") // Seperate API. The API Will Be Seperated if the Smart-API is Complete.
 include("Smart-BukkitAPI") // an Official Name For Legacy API Updates.
