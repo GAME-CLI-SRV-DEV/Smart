976bb9bf0308f8c850ca1df1122785704bcac622
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-01-11 04:13:41 +0900

    Update build.gradle.kts

--- SmartModLoader/build.gradle.kts
+++ SmartModLoader/build.gradle.kts
@@ -6,7 +6,6 @@ tasks.processResources {
     val apiVersion = rootProject.providers.gradleProperty("mcVersion").get()
     val props = mapOf(
         "version" to project.version,
-        "apiversion" to "\"$apiVersion\"",
     )
     inputs.properties(props)
     filesMatching("plugin.yml") {
