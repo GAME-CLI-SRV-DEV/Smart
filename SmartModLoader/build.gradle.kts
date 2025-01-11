86459b497f9570aed358c7c7332d35ee07a52122
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-01-11 14:39:43 +0900

    Update build.gradle.kts

--- SmartModLoader/build.gradle.kts
+++ SmartModLoader/build.gradle.kts
@@ -1,11 +1,12 @@
 dependencies {
     compileOnly(project(":Smart-BukkitAPI"))
+    
 }
 
 tasks.processResources {
     val apiVersion = rootProject.providers.gradleProperty("mcVersion").get()
     val props = mapOf(
-        "version" to project.version,
+        "version" to rootProject.providers.gradleProperty("version").get(),
     )
     inputs.properties(props)
     filesMatching("plugin.yml") {
