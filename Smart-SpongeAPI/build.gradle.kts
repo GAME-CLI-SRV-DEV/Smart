6c6f9525f77f0a1a33e5ab649b2dd651834d0824
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-01-11 15:02:04 +0900

    Update build.gradle.kts

--- Smart-SpongeAPI/build.gradle.kts
+++ Smart-SpongeAPI/build.gradle.kts
@@ -1,1 +1,13 @@
+val version = rootProject.providers.gradleProperty("version").get()
 
+repositories {
+    mavenCentral()
+    maven {
+        name = 'sponge-repo'
+        url = 'https://repo.spongepowered.org/repository/maven-public/'
+    }
+}
+
+dependencies {
+    implementation("org.spongepowered:spongeapi:" + version)
+}
